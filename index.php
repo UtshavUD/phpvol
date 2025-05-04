7a4aa514a00970797de21f16ab92c944b558928b
Kanchan <kanchandas316@gmail.com>
2025-05-04 15:46:04 +0545

    collabb

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<h1>Sudurpaschim Nepal!</h1>';
-echo '<h1>HI,I am Utshav Dahal from Biratnagar Nepal!</h1>';
+echo '<h1>HI,I am Kanchan Das from Nepal!</h1>';
 echo'<img src="mnr.jpg"width="700">';
 ?>
