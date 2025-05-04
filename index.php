7106d2c62261bcf63e2948f78562d55b2f3ad78d
Utshav Dahal <utshav.dahalk99@gmail.com>
2025-05-04 12:51:19 +0545

    Added my intro

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 echo '<h1>Sudurpaschim Nepal!</h1>';
-echo '<h1>Biratnagar Nepal!</h1>';
+echo '<h1>HI,I am Utshav Dahal from Biratnagar Nepal!</h1>';
 echo'<img src="mnr.jpg"width="700">';
-echo'<img src="profile.png"width="700">';
 ?>
